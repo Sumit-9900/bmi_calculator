d7971dbfacb8b730b163b0c400ac767d1dcb3941
Sumit-9900 <paulsumit9900@gmail.com>
2023-12-19 17:30:02 +0530

    changed gender code

--- lib/widgets/gender.dart
+++ lib/widgets/gender.dart
@@ -1,81 +1,69 @@
 import 'package:flutter/material.dart';
 
-class Gender extends StatelessWidget {
-  const Gender({super.key});
+// ignore: must_be_immutable
+class Gender extends StatefulWidget {
+  Gender({
+    super.key,
+    required this.text,
+    required this.icon,
+    required this.isMaleSelected,
+    required this.isFemaleSelected,
+  });
+  final IconData icon;
+  final String text;
+  bool isMaleSelected;
+  bool isFemaleSelected;
 
   @override
+  State<Gender> createState() => _GenderState();
+}
+
+class _GenderState extends State<Gender> {
+  bool isButtonPressed = false;
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 160.0,
-          width: 160.0,
-          margin: const EdgeInsets.only(
-            left: 20.0,
-            right: 10.0,
-            top: 20.0,
-            bottom: 20.0,
-          ),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(60, 22, 86, 197),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: const Icon(
-                  Icons.male,
-                  size: 85.0,
-                ),
-                onPressed: () {},
-                color: Colors.white70,
-              ),
-              const Text(
-                'MALE',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          // isButtonPressed = !isButtonPressed;
+          widget.isMaleSelected = !widget.isMaleSelected;
+          widget.isFemaleSelected = !widget.isFemaleSelected;
+        });
+      },
+      child: Container(
+        height: 160.0,
+        width: 160.0,
+        margin: const EdgeInsets.only(
+          left: 20.0,
+          right: 10.0,
+          top: 20.0,
+          bottom: 20.0,
         ),
-        Container(
-          height: 160.0,
-          width: 160.0,
-          margin: const EdgeInsets.only(
-            left: 20.0,
-            right: 10.0,
-            top: 20.0,
-            bottom: 20.0,
-          ),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(60, 22, 86, 197),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: const Icon(
-                  Icons.female,
-                  size: 85.0,
-                ),
-                onPressed: () {},
-                color: Colors.white70,
-              ),
-              const Text(
-                'FEMALE',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
+        decoration: BoxDecoration(
+          color: widget.isMaleSelected
+              ? const Color.fromARGB(60, 22, 86, 197)
+              : const Color.fromARGB(57, 150, 179, 228),
+          borderRadius: BorderRadius.circular(10.0),
         ),
-      ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              widget.icon,
+              size: 85.0,
+              color: widget.isMaleSelected ? Colors.white70 : Colors.black87,
+            ),
+            Text(
+              widget.text,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: widget.isMaleSelected ? Colors.white : Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
